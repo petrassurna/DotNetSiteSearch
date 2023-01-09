0ded32766150cbc1f32ba6336e19b467f8ea8c09
Petras Surna <petras.surna@yart.com.au>
2023-01-09 17:30:38 +1100

    add renamed SearchResults/UriSearchResult.cs

---
+++ Searchable/SearchResults/UriSearchResultToReturn.cs
@@ -0,0 +1,18 @@
+﻿using Searchable.Stemming;
+using System.Text.RegularExpressions;
+
+namespace Searchable.SearchResults
+{
+  public class UriSearchResultToReturn
+  {
+    public string Id { get; set; } = null!;
+
+    public string Path { get; set; } = null!;
+
+    public double Score { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public IEnumerable<string> ContentMatches { get; set; } = null!;
+  }
+}
